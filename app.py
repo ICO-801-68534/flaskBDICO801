fda8319b82d58cce8a4effccd416cee16892cc29
ICO-801-68534 <68534leo@universidaddeleon.edu.mx>
2026-02-13 09:50:32 -0600

    Formularios 13/02/2026

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/usuarios")
+def usuarios():
+	return render_template("usuarios.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
